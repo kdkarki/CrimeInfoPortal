93dd255044823d98beb7a6c6c49a0989c19c22cd
kdk560@vt.edu <kdk560@vt.edu>
2015-03-23 16:15:49 -0400

    Adding Data Access Layer and Web service implementation
    1. DAL is designed using ADO.Net Entity framework. It is mapped directly to production database.
    2. Web service is designed using WCF. The responses are returned using xml.

---
+++ src/BLL/IService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using DAL;
+
+namespace BLL
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService
+    {
+
+        [OperationContract]
+        [WebInvoke(Method="GET", 
+                    ResponseFormat=WebMessageFormat.Xml, 
+                    BodyStyle=WebMessageBodyStyle.Wrapped, 
+                    UriTemplate = "GetData/{value}")]
+        string GetData(string value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+                    ResponseFormat = WebMessageFormat.Xml,
+                    UriTemplate = "GetUserByUserName/{username}")]
+        User GetUserByUserName(string UserName);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+                    ResponseFormat = WebMessageFormat.Xml,
+                    RequestFormat = WebMessageFormat.Xml,
+                    BodyStyle = WebMessageBodyStyle.Wrapped,
+                    UriTemplate = "AuthenticateUser?username={username}&password={password}")]
+        bool AuthenticateUser(string Username, string Password);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+                    ResponseFormat = WebMessageFormat.Xml,
+                    RequestFormat = WebMessageFormat.Xml,
+                    BodyStyle = WebMessageBodyStyle.Wrapped,
+                    UriTemplate = "CreateUser")]
+        bool CreateNewUser(DAL.User NewUser);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ src/BLL/Service.svc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using DAL;
+
+namespace BLL
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service : IService
+    {
+        public string GetData(string value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public DAL.User GetUserByUserName(string UserName)
+        {
+            CrimeInfoPortalDAO objDAO = new CrimeInfoPortalDAO();
+            return objDAO.Users.FirstOrDefault(u => u.Username == UserName);
+        }
+
+        public bool AuthenticateUser(string Username, string Password)
+        {
+            CrimeInfoPortalDAO objDAO = new CrimeInfoPortalDAO();
+            if (objDAO.Users.FirstOrDefault(u => u.Username == Username && u.Password == Password) != null)
+                return true;
+            return false;
+        }
+
+        public bool CreateNewUser(DAL.User NewUser)
+        {
+            CrimeInfoPortalDAO objDAO = new CrimeInfoPortalDAO();
+            objDAO.AddToUsers(NewUser);
+            try
+            {
+                int saveResult = objDAO.SaveChanges();
+                if (saveResult == 0)
+                    return false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
